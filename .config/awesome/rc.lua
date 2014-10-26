47df61a665db0b92f233220a00775bf12075a786
Hersh Singh <hershdeep@gmail.com>
2014-10-25 20:37:06 -0400

    Enable 'run' prompt.

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -451,7 +451,7 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey, "Control" }, "n", awful.client.restore),
 
     -- Prompt
-    --awful.key({ modkey },            "r",     function () mypromptbox[mouse.screen]:run() end),
+    awful.key({ modkey },            "r",     function () mypromptbox[mouse.screen]:run() end),
 
      --Awesome MenuBar
     --awful.key({ modkey }, "p", function () menubar.show() end),
