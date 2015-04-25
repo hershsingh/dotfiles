5d5ab883a72211ef27a95d5ca030e1ecaeb8232b
Hersh Singh <hershdeep@gmail.com>
2015-04-25 17:54:00 -0400

    Colored man pages in less

--- .zsh/colors.zsh
+++ .zsh/colors.zsh
@@ -20,3 +20,15 @@ export PR_BOLD_WHITE PR_BOLD_BLACK
 
 export CLICOLOR=1
 #export LS_COLORS=exfxcxdxbxegedabagacad
+
+# Colored man pages in less
+man() {
+    env LESS_TERMCAP_mb=$'\E[01;31m' \
+    LESS_TERMCAP_md=$'\E[01;38;5;74m' \
+    LESS_TERMCAP_me=$'\E[0m' \
+    LESS_TERMCAP_se=$'\E[0m' \
+    LESS_TERMCAP_so=$'\E[38;5;246m' \
+    LESS_TERMCAP_ue=$'\E[0m' \
+    LESS_TERMCAP_us=$'\E[04;38;5;146m' \
+    man "$@"
+}
