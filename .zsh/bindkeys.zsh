f1a20e27d0a9030b4a17555dcac84054b448484f
Hersh Singh <hershdeep@gmail.com>
2015-06-13 17:21:56 -0400

    Keybindings for history substring search plugin

--- .zsh/bindkeys.zsh
+++ .zsh/bindkeys.zsh
@@ -24,3 +24,23 @@ bindkey '^?' backward-delete-char
 bindkey '^H' backward-delete-char
 bindkey '^F'      vi-forward-word                         # ctrl-f
 #bindkey '^I'      end-of-line                          # ctrl-e
+
+
+# History substring search plugin
+# bind UP and DOWN arrow keys
+zmodload zsh/terminfo
+bindkey "$terminfo[kcuu1]" history-substring-search-up
+bindkey "$terminfo[kcud1]" history-substring-search-down
+
+# bind UP and DOWN arrow keys (compatibility fallback
+# for Ubuntu 12.04, Fedora 21, and MacOSX 10.9 users)
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+
+# bind P and N for EMACS mode
+bindkey -M emacs '^P' history-substring-search-up
+bindkey -M emacs '^N' history-substring-search-down
+
+# bind k and j for VI mode
+bindkey -M vicmd 'k' history-substring-search-up
+bindkey -M vicmd 'j' history-substring-search-down
