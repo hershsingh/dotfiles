c68e3393069b8f95183a27032759d33f1702830a
Hersh Singh <hershdeep@gmail.com>
2016-09-09 20:34:23 -0400

    Do not track yas-compiled-snippets

--- .emacs.d/snippets/fundamental-mode/.yas-compiled-snippets.el
+++
@@ -1,8 +0,0 @@
-;;; Compiled snippets and support files for `fundamental-mode'
-;;; Snippet definitions:
-;;;
-(yas-define-snippets 'fundamental-mode
-                     '(("date" "`(insert (format-time-string \"[%B %d, %Y, %A]\"))`" "date" nil nil nil "/home/hersh/.emacs.d/snippets/fundamental-mode/date.yasnippet" nil nil)))
-
-
-;;; Do not edit! File generated at Tue Jun 14 16:38:17 2016
